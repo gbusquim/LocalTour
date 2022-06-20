8577ef71ee1209dd116f38143c9a1495c1c7c7ed
mcostalonga <marcelo.costalonga.cardoso@gmail.com>
2022-06-20 19:29:04 -0400

    [To-Fix] New TODOs

---
+++ LocalTour/model/OnwerObserver.swift
@@ -0,0 +1,8 @@
+//
+//  OnwerObserver.swift
+//  LocalTour
+//
+//  Created by user221960 on 6/20/22.
+//
+
+import Foundation
--- LocalTour/model/Place.swift
+++ LocalTour/model/Place.swift
@@ -11,11 +11,12 @@ class Place {
     var id: Int
     var name: String
     var description: String
-    var score: Float
+    var score: Float // TODO: deixar de acordo com o diagrama da arq (avgRating) -> Ou mudar no diagrama pra ficar igual aqui, o importante eh manter a consistencia
     var phoneNumber: String
     var adress: String?
 //    var cnpj: String // TODO: add cnpj
 //    var owner: Owner // TODO: add owner
+//    var userReviews: [Review] // TODO: add reviews...
     
     // TODO: Fix this, need to create subtypes classes...
     var imgTypePath: String?
