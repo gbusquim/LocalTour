56c7befac798a1fe65edcd5195de6cc2c769db27
Gabriel <gbtatu@hotmail.com>
2022-07-03 21:44:20 -0400

    CollectionView for Reviews

--- LocalTour/controller/PlaceViewController.swift
+++ LocalTour/controller/PlaceViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-class PlaceViewController: UIViewController,
-                           UITableViewDelegate, UITableViewDataSource{
+class PlaceViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
    
 
     //TODO: Usar outro dado que nao o nome do lugar escolhido?
@@ -21,7 +20,8 @@ class PlaceViewController: UIViewController,
 
     var sustainableImages = [UIImageView]()
     
-    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -34,8 +34,10 @@ class PlaceViewController: UIViewController,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
         self.dao = DaoMemory.getInstance()
         places = (self.dao?.getAllPlaces())!
 
@@ -82,16 +84,36 @@ class PlaceViewController: UIViewController,
         // Pass the selected object to the new view controller.
     }
     */
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.reviews.count
-    }
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.reviews.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell", for: indexPath) as! ReviewTableViewCell
+//        let review = self.reviews[indexPath.row]
+//        cell.reviewComment.text = review.text
+//        cell.reviewerName.text = review.author
+//        cell.reviewScore.text = review.score.description
+//        return cell
+//    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reviewCell", for: indexPath) as! ReviewTableViewCell
-        let review = self.reviews[indexPath.row]
-        cell.reviewComment.text = review.text
-        cell.reviewerName.text = review.author
-        cell.reviewScore.text = review.score.description
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+//        return Owner.places.count // TODO: use owner.places
+        return 5  // TODO: use DataDemo instead
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReview", for: indexPath) as! PlaceReviewsCollectionViewCell
+        
+//        cell.IconBigPlaceType.image = UIImage(named: DataPlaces.places[indexPath.row].imgName ?? "bed-big")
+//        cell.lblPlaceName.text = DataPlaces.places[indexPath.row].name
+        cell.userTextReview.text = "TESTE"
+        cell.startReviewImage.image = UIImage(named: "bed-big")
+        cell.userRating.text = "5"
+        cell.userName.text = "Jorge"
+        // Configure the cell
+    
         return cell
     }
     
---
+++ LocalTour/view/PlaceReviewsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceReviewsCollectionViewCell.swift
+//  LocalTour
+//
+//  Created by user221918 on 7/3/22.
+//
+
+import UIKit
+
+class PlaceReviewsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var userTextReview: UILabel!
+    @IBOutlet weak var startReviewImage: UIImageView!
+    @IBOutlet weak var userRating: UILabel!
+    @IBOutlet weak var userName: UILabel!
+}
