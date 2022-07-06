e77e909ee15107ea9d70fb9aa4030c291efb83d2
Gabriel <gbtatu@hotmail.com>
2022-07-06 00:54:19 -0400

    alteração de pasta para view cell

---
+++ LocalTour/controller/viewCells/OwnerPlacesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  OwnerPlacesCollectionViewCell.swift
+//  LocalTour
+//
+//  Created by user221960 on 6/20/22.
+//
+
+import UIKit
+
+class OwnerPlacesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblPlaceName: UILabel!
+    @IBOutlet weak var IconBigPlaceType: UIImageView!
+}
